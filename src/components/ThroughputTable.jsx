517d5867e98c96a0b86eb6268baa5562248f8bec
Henna Mäkiluoma <henna.makiluoma@gmail.com>
2026-01-18 14:58:57 +0200

    Add tables to adminUI

---
+++ src/components/ThroughputTable.jsx
@@ -0,0 +1,362 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { visuallyHidden } from '@mui/utils';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import IconButton from '@mui/material/IconButton';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+const columns = [
+  { id: 'vartio', label: 'Vartio', minWidth: 170 },
+  {
+    id: 'sarja',
+    label: 'Sarja',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'rasti',
+    label: 'Rasti',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'saapumisaika',
+    label: 'Saapumisaika',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString(),
+  },
+  {
+    id: 'poistumisaika',
+    label: 'Poistumisaika',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString(),
+  },
+  {
+    id: 'aika_rastilla',
+    label: 'Aika rastilla',
+    minWidth: 170,
+    align: 'right',
+  },
+];
+
+const timeDifference = (date1, date2) => {
+    var difference = date1.getTime() - date2.getTime();
+
+    var daysDifference = Math.floor(difference/1000/60/60/24);
+    difference -= daysDifference*1000*60*60*24
+
+    var hoursDifference = Math.floor(difference/1000/60/60);
+    difference -= hoursDifference*1000*60*60
+
+    var minutesDifference = Math.floor(difference/1000/60);
+    difference -= minutesDifference*1000*60
+
+    var secondsDifference = Math.floor(difference/1000);
+
+    return daysDifference > 0
+              ? daysDifference + 'pv, ' + hoursDifference + 'h, ' + minutesDifference + 'min, ' + secondsDifference + 's'
+              :  hoursDifference + 'h, ' + minutesDifference + 'min, ' + secondsDifference + 's';
+}
+
+const createData = (vartio, sarja, rasti, saapumisaika, poistumisaika) => {
+  const aika = poistumisaika ? timeDifference(poistumisaika, saapumisaika) : '-';
+  return { vartio, sarja, rasti, saapumisaika, poistumisaika: poistumisaika ? poistumisaika : '-', aika_rastilla: aika };
+}
+
+const descendingComparator = (a, b, orderBy) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+const getComparator = (order, orderBy) => {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+const rows = [
+  createData('Manaatit', 'sininen', 1, new Date("2026-01-18T09:20:00"), new Date("2026-01-18T10:10:00")),
+  createData('Undulaatit', 'punainen', 1, new Date("2026-01-18T09:45:00"), new Date("2026-01-18T10:50:00")),
+  createData('Manaatit', 'sininen', 2, new Date("2026-01-18T10:24:00")),
+];
+
+const EnhancedTableHead = (props) => {
+  const {order, orderBy, onRequestSort} = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {columns.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+  const {
+    onRequestFilterVisibility,
+    isVisible,
+    onSarjaFilterChange,
+    sarjaFilter,
+    onRastiFilterChange,
+    rastiFilter,
+    onVartioFilterChange,
+    vartioFilter,
+    onClearFilters
+  } = props;
+
+  const changeFilterVisibility = () => onRequestFilterVisibility();
+
+  const handleSarjaFilterChange = (event) => onSarjaFilterChange(event.target.value);
+
+  const handleRastiFilterChange = (event) => onRastiFilterChange(event.target.value);
+
+  const handleVartioFilterChange = (event) => onVartioFilterChange(event.target.value);
+
+  const clearFilters = () => onClearFilters();
+
+  return (
+    <Toolbar
+      sx={[
+        {
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+        }
+      ]}
+    >
+      <Tooltip title="Filter list">
+        <IconButton onClick={changeFilterVisibility}>
+          <FilterListIcon />
+        </IconButton>
+      </Tooltip>
+      {isVisible &&
+      <Box sx={{ display: "flex", gap: "10px", margin: "10px"}}>
+        <FormControl sx={{ minWidth: 120 }} size="small">
+          <InputLabel id="sarja-filter">Sarja</InputLabel>
+          <Select
+            labelId="sarja-filter"
+            id="sarja-filter"
+            value={sarjaFilter}
+            label="Sarja"
+            onChange={handleSarjaFilterChange}
+          >
+            <MenuItem value={''}>Kaikki sarjat</MenuItem>
+            <MenuItem value={'sininen'}>Sininen</MenuItem>
+            <MenuItem value={'punainen'}>Punainen</MenuItem>
+            <MenuItem value={'ruskea'}>Ruskea</MenuItem>
+            <MenuItem value={'harmaa'}>Harmaa</MenuItem>
+            <MenuItem value={'keltainen'}>Keltainen</MenuItem>
+          </Select>
+          
+        </FormControl>
+        <FormControl sx={{ minWidth: 120 }} size="small">
+          <InputLabel id="rasti-filter">Rasti</InputLabel>
+          <Select
+            labelId="rasti-filter"
+            id="rasti-filter"
+            value={rastiFilter}
+            label="Rasti"
+            onChange={handleRastiFilterChange}
+          >
+            <MenuItem value={0}>Kaikki rastit</MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          size="small"
+          id="vartio-filter"
+          label="Vartio"
+          variant="outlined"
+          value={vartioFilter}
+          onChange={handleVartioFilterChange}
+        />
+        <Button variant="outlined" onClick={clearFilters}>Tyhjennä valinnat</Button>
+      </Box>
+      }
+    </Toolbar>
+  );
+}
+
+EnhancedTableToolbar.propTypes = {
+  onRequestFilterVisibility: PropTypes.func.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+  onSarjaFilterChange: PropTypes.func.isRequired,
+  sarjaFilter: PropTypes.string.isRequired,
+  onRastiFilterChange: PropTypes.func.isRequired,
+  rastiFilter: PropTypes.number.isRequired,
+  onVartioFilterChange: PropTypes.func.isRequired,
+  vartioFilter: PropTypes.string.isRequired,
+  onClearFilters: PropTypes.func.isRequired
+};
+
+const ThroughputTable = () => {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('vartio');
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [filtersVisible, setFiltersVisible] = React.useState(false);
+  const [sarjaFilter, setSarjaFilter] = React.useState('');
+  const [rastiFilter, setRastiFilter] = React.useState(0);
+  const [vartioFilter, setVartioFilter] = React.useState('');
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleFilterVisibility = () => {
+    setFiltersVisible(!filtersVisible);
+  };
+
+  const handleSarjaFilterChange = (newFilter) => {
+    setSarjaFilter(newFilter);
+  }
+
+  const handleRastiFilterChange = (newFilter) => {
+    setRastiFilter(newFilter);
+  }
+
+  const handleVartioFilterChange = (newFilter) => {
+    setVartioFilter(newFilter);
+  }
+
+  const clearFilters = () => {
+    setSarjaFilter('');
+    setRastiFilter(0);
+    setVartioFilter('');
+  }
+
+  const visibleRows = React.useMemo(
+    () =>
+      [...rows]
+        .sort(getComparator(order, orderBy))
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [order, orderBy, page, rowsPerPage],
+  );
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <EnhancedTableToolbar
+        onRequestFilterVisibility={handleFilterVisibility}
+        isVisible={filtersVisible}
+        onSarjaFilterChange={handleSarjaFilterChange}
+        sarjaFilter={sarjaFilter}
+        onRastiFilterChange={handleRastiFilterChange}
+        rastiFilter={rastiFilter}
+        onVartioFilterChange={handleVartioFilterChange}
+        vartioFilter={vartioFilter}
+        onClearFilters={clearFilters}
+      />
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+          <TableBody>
+            {visibleRows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .filter((row) =>
+                row.sarja.includes(sarjaFilter) &&
+                (rastiFilter === 0 ? true : row.rasti === rastiFilter) &&
+                row.vartio.toLowerCase().includes(vartioFilter.toLowerCase())
+              )
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+export default ThroughputTable
--- src/pages/AdminUI.jsx
+++ src/pages/AdminUI.jsx
@@ -1,11 +1,74 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
 import ThroughputTable from "../components/ThroughputTable"
+import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import CheckpointTable from '../components/CheckpointTable';
+
+const CustomTabPanel = (props) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 const AdminUI = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <>
-      <p>Admin UI</p>
-      <ThroughputTable />
-    </>
+     <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Vartiot" {...a11yProps(0)} />
+          <Tab label="Rastit" {...a11yProps(1)} />
+          <Tab label="Rastien väliset matkat" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Typography variant='h2' gutterBottom component={'h1'}>
+          Vartioiden eteneminen
+        </Typography>
+        <ThroughputTable />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Typography variant='h2' gutterBottom component={'h1'}>
+          Rastien eteneminen
+        </Typography>
+        <CheckpointTable />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Two
+      </CustomTabPanel>
+    </Box>
   )
 }
 
